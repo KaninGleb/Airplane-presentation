ba7cc11272c87e11973ca64cf6a89fd26089d6ac
Kanin Gleb <gleb.kanin@gmail.com>
2025-08-04 14:33:47 +0300

    style: format code with Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
-import React, {Suspense, useRef} from 'react';
-import {Canvas, useFrame} from '@react-three/fiber';
-import {OrbitControls, useGLTF} from '@react-three/drei';
+import React, { Suspense, useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls, useGLTF } from '@react-three/drei'
 
 function Airplane(props) {
-  const {scene} = useGLTF('/greenPlane.glb');
-  const ref = useRef();
+  const { scene } = useGLTF('/greenPlane.glb')
+  const ref = useRef()
 
   useFrame((state, delta) => {
     if (ref.current) {
-      ref.current.rotation.y += delta * 0.3;
+      ref.current.rotation.y += delta * 0.3
     }
-  });
+  })
 
-  return <primitive ref={ref} object={scene} {...props} />;
+  return <primitive ref={ref} object={scene} {...props} />
 }
 
 export default function App() {
   return (
-    <div style={{width: '100vw', height: '100vh', background: '#303030'}}>
-      <Canvas camera={{position: [0, 2, 10], fov: 75}}>
+    <div style={{ width: '100vw', height: '100vh', background: '#303030' }}>
+      <Canvas camera={{ position: [0, 2, 10], fov: 75 }}>
         <Suspense fallback={null}>
-          <ambientLight intensity={1.5}/>
-          <directionalLight position={[10, 10, 5]} intensity={1.5}/>
+          <ambientLight intensity={1.5} />
+          <directionalLight position={[10, 10, 5]} intensity={1.5} />
 
-          <Airplane scale={0.5} position={[0, 0, 0]}/>
+          <Airplane scale={0.4} position={[0, 0, 0]} />
 
-          <OrbitControls/>
+          <OrbitControls />
         </Suspense>
       </Canvas>
     </div>
-  );
+  )
 }
